ea80cfa06b3986b4d1748def4258b366c05702b7
alexis39tt <alessandro.fonzi@gmail.com>
2022-09-15 13:14:13 +0200

    finished api call page

--- src/components/ApiCall.jsx
+++ src/components/ApiCall.jsx
@@ -1,5 +1,25 @@
-function ApiCall(){
-    return <h1>ApiCall</h1>
+import axios from "axios";
+import "bulma/css/bulma.css";
+import NewsCard from "./NewsCard";
+import "./NewsCard.css"
+import { useEffect, useState } from "react";
+
+function ApiCall() {
+    let [news, setNews] = useState(null)
+    async function getNews() {
+        await axios.get("https://inshorts.deta.dev/news?category=technology")
+            .then((res) => {
+                setNews(res.data)
+            })
+    }
+    useEffect(() => {
+        getNews();
+    }, [])
+
+    return (<>
+        {!news && <div className="spinner"><div className="lds-ring"><div></div><div></div><div></div><div></div></div></div>}
+        {news && news.data.map((el, i) => <NewsCard info={el} key={i} />)}
+    </>);
 }
 
 export default ApiCall;
---
+++ src/components/NewsCard.jsx
@@ -0,0 +1,23 @@
+import "bulma/css/bulma.css";
+import "./NewsCard.css";
+
+function NewsCard(props) {
+    return (<a href={props.info.url}>
+        <div className="card">
+            <div className="card-header">
+                <figure className="image is-128x128">
+                    <img src={props.info.imageUrl} alt={props.info.title} />
+                </figure>
+                <div>
+                <h1 className="title">{props.info.title}</h1>
+                <h2 className="author is-size-4">di {props.info.author} - {props.info.date}</h2>
+                </div>
+            </div>
+            <div className="card-content">
+                <p>{props.info.content}</p>
+            </div>
+        </div>
+    </a>);
+}
+
+export default NewsCard;
