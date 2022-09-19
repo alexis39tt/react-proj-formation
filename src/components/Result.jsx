50cfaa20cd887b8005e9292001e9b157a5cc5748
alexis39tt <alessandro.fonzi@gmail.com>
2022-09-19 18:16:03 +0200

    handled loading form result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
+
 function Result() {
-    let [result, setResult] = useState('a')
+    let [result, setResult] = useState(null)
     const location = useLocation();
 
     useEffect(() => {
         setResult(location.state.values)
     }, [])
 
-    return (
-        <div className="card">
-            <h1 className="is-size-3"><span className="title">Nome: </span>{result.name}</h1>
-            <h1 className="is-size-3"><span className="title">Età: </span>{result.age ? "Maggiorenne" : "Minorenne"}</h1>
-            <h1 className="is-size-3"><span className="title">Genere: </span>{result.gender == "male" ? "Maschio" : "Femmina"}</h1>
-        </div>
-    )
+    return (<>
+        {!result && <h1>Loading...</h1>}
+        {result &&
+            <div className="card">
+                <h1 className="is-size-3"><span className="title">Nome: </span>{result.name}</h1>
+                <h1 className="is-size-3"><span className="title">Età: </span>{result.age ? "Maggiorenne" : "Minorenne"}</h1>
+                <h1 className="is-size-3"><span className="title">Genere: </span>{result.gender == "male" ? "Maschio" : "Femmina"}</h1>
+            </div>}
+        </>)
 }
 
 export default Result;
